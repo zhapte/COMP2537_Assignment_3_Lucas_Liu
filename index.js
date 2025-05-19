0c3f42d517185c3a1c53fc2879f2ad9ba9bda1a9
Lucas Liu <lucasqhliu@gmail.com>
2025-05-18 19:36:50 -0700

    initial creation of the git repo

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+
+function setup () {
+  let firstCard = undefined
+  let secondCard = undefined
+  $(".card").on(("click"), function () {
+    $(this).toggleClass("flip");
+
+    if (!firstCard)
+      firstCard = $(this).find(".front_face")[0]
+    else {
+      secondCard = $(this).find(".front_face")[0]
+      console.log(firstCard, secondCard);
+      if (
+        firstCard.src
+        ==
+        secondCard.src
+      ) {
+        console.log("match")
+        $(`#${firstCard.id}`).parent().off("click")
+        $(`#${secondCard.id}`).parent().off("click")
+      } else {
+        console.log("no match")
+        setTimeout(() => {
+          $(`#${firstCard.id}`).parent().toggleClass("flip")
+          $(`#${secondCard.id}`).parent().toggleClass("flip")
+        }, 1000)
+      }
+    }
+  });
+}
+
+$(document).ready(setup)
